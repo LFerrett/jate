20f379a625f2f859c7a534d9324fbf41913c914c
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-14 21:27:37 -0500

    Updated method to get all content from database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, content: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get().content;
   const result = await request;
   console.log(result);
 };
